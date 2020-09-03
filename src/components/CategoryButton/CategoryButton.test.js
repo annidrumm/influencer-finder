a050602d88950ad6df437f850bb85f5c16baaa63
Anni <anniiwhere@gmail.com>
2020-09-03 14:46:02 +0200

    Add test for CategoryButton

---
+++ src/components/CategoryButton/CategoryButton.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import CategoryButton from './CategoryButton'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('CategoryButton', () => {
+  it('renders CategoryButton correctly', () => {
+    const tree = renderer.create(<CategoryButton />)
+    expect(tree).toMatchSnapshot()
+  })
+  it('renders subtractButton correctly', () => {
+    const tree = renderer.create(<CategoryButton />)
+    expect(tree).toMatchSnapshot()
+  })
+})

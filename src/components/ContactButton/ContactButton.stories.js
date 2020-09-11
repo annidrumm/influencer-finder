5ad8ce79030af024ba029e3f0f4eced2b66e04fd
Anni <anniiwhere@gmail.com>
2020-09-11 17:40:12 +0200

    Update storybook

--- src/components/ContactButton/ContactButton.stories.js
+++ src/components/ContactButton/ContactButton.stories.js
@@ -4,12 +4,8 @@ import ContactButton from './ContactButton'
 export default {
   title: 'Components/Button',
   component: ContactButton,
-  argTypes: {
-    headline: { control: 'text' },
-  },
 }
 
-const Template = (args) => <ContactButton {...args} />
-
-export const contact = Template.bind({})
-contact.args = {}
+export const contact = () => {
+  return <ContactButton />
+}

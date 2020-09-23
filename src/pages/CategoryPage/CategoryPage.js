9017c0426d2304572a3ae9945cbed0541ebd8509
Anni <anniiwhere@gmail.com>
2020-09-23 22:30:53 +0200

    Fix CategoryButtons

--- src/pages/CategoryPage/CategoryPage.js
+++ src/pages/CategoryPage/CategoryPage.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import styled from 'styled-components/macro'
 import CategoryButtons from '../../components/CategoryButtons/CategoryButtons'
 import HeaderCategoryPage from '../../components/Header/HeaderCategoryPage'
 
 export default function CategoryPage() {
   return (
-    <ContainerStyled>
+    <section>
       <HeaderCategoryPage headline="Categories" />
       <CategoryButtons />
-    </ContainerStyled>
+    </section>
   )
 }
-
-const ContainerStyled = styled.section`
-  text-align: center;
-  margin: 30px;
-`

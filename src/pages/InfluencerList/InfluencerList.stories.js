cf9201672f9376427c53be59a679c2cb6d2a16db
Anni <anniiwhere@gmail.com>
2020-09-30 19:54:33 +0200

    Add FavouritePage to storybook

--- src/pages/InfluencerList/InfluencerList.stories.js
+++ src/pages/InfluencerList/InfluencerList.stories.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import InfluencerList from './InfluencerList'
 import influencerData from '../../mocks/influencer.json'
+import HeaderInfluencerList from '../../components/Header/HeaderInfluencerList'
 
 export default {
   title: 'Pages/InfluencerList',
@@ -8,5 +9,10 @@ export default {
 }
 
 export const fullList = () => {
-  return <InfluencerList influencerData={influencerData} />
+  return (
+    <>
+      <HeaderInfluencerList headline="Browse" />
+      <InfluencerList influencerData={influencerData} />
+    </>
+  )
 }
